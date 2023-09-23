88507ce3726e356df8b6a7948adfab53eadeb747
tatanadubova <tatyana.dubova.1986@gmail.com>
2023-09-23 21:15:06 +0300

    Added HomeWork2

--- SwiftBookCourse/ViewController.swift
+++ SwiftBookCourse/ViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redTrafficLight: UIView!
+    @IBOutlet weak var orangeTrafficLight: UIView!
+    @IBOutlet weak var greenTrafficLight: UIView!
+    @IBOutlet weak var colorSelectionButton: UIButton!
+    
+    private var color: Colors = .none
+    
+    private enum Colors {
+        case none
+        case red
+        case orange
+        case green
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redTrafficLight.backgroundColor = .systemRed
+        orangeTrafficLight.backgroundColor = .systemOrange
+        greenTrafficLight.backgroundColor = .systemGreen
+        
+        updateTrafficLight()
+        
+        colorSelectionButton.layer.cornerRadius = 20
+        colorSelectionButton.setTitle("START", for: .normal)
     }
-
-
+    
+    @IBAction func colorSelectionButtonPressed(_ sender: UIButton) {
+        sender.setTitle("NEXT", for: .normal)
+        next()
+        updateTrafficLight()
+    }
+    
+    private func next() {
+        switch color {
+        case .red:
+            color = .orange
+        case .orange:
+            color = .green
+        case .green, .none:
+            color = .red
+        }
+    }
+    
+    private func updateTrafficLight() {
+        switch color {
+        case .red:
+            redTrafficLight.alpha = 1
+            orangeTrafficLight.alpha = 0.3
+            greenTrafficLight.alpha = 0.3
+        case .orange:
+            orangeTrafficLight.alpha = 1
+            redTrafficLight.alpha = 0.3
+            greenTrafficLight.alpha = 0.3
+        case .green:
+            greenTrafficLight.alpha = 1
+            redTrafficLight.alpha = 0.3
+            orangeTrafficLight.alpha = 0.3
+        case .none:
+            greenTrafficLight.alpha = 0.3
+            redTrafficLight.alpha = 0.3
+            orangeTrafficLight.alpha = 0.3
+        }
+    }
+    
+    private func updateCornerRadius() {
+        let width = redTrafficLight.frame.width
+        redTrafficLight.layer.cornerRadius = width / 2
+        orangeTrafficLight.layer.cornerRadius = width / 2
+        greenTrafficLight.layer.cornerRadius = width / 2
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateCornerRadius()
+    }
 }
-
